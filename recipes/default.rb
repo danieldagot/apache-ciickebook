2e8f6db852f190fdd41fb0005d3ffb72a544af83
danieldagot <danieldagot@gmail.com>
2021-04-20 20:15:32 +0300

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+package 'apache2'
+
+file '/var/www/html/index.html' do
+  content "<h2>This is: #{node['name']}</h2><h1>Hello ChefConf2019!!</h1>"
+end
+
+service 'apache2' do
+  action [ :enable, :start ]
+end

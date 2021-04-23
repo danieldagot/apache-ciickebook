84af1a2329238731a335a90bd10e2beb41167bab
danieldagot <51596364+danieldagot@users.noreply.github.com>
2021-04-24 00:49:17 +0300

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,16 +1,19 @@
-#
-# Cookbook:: apache
-# Recipe:: default
-#
-# Copyright:: 2021, The Authors, All Rights Reserved.
-
+execute "update-upgrade" do
+    command "sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' upgrade"
+      action :run
+end
 package 'apache2'
 
 file '/var/www/html/index.html' do
-  content "<h2>This is: #{node['name']}</h2><h1>Hello ChefConf2019!!</h1> danil"
-  
+  content " Hello#{node['username']} <p id='date'></p>
+  <script>
+  n =  new Date();
+  y = n.getFullYear();
+  m = n.getMonth() + 1;
+  d = n.getDate();
+  document.getElementById('date').innerHTML = Date();
+  </script>"
 end
-
 service 'apache2' do
   action [ :enable, :start ]
 end
